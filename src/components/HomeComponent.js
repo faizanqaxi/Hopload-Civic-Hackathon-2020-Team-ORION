56e08256b44cd45d6dc593e4c93bbb455b0f6996
faizanqaxi <faizanqaxi78@gmail.com>
2020-12-09 03:33:42 +0500

    Fixed Background Bug

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Home(props) {
     return (
-        <div className="container">
+        <div className="container-home">
             <div className="home">
                 <div className="row space">
                     <div className="col-12">
@@ -15,6 +15,12 @@ function Home(props) {
                         <h1> <span className="otherclr">Find </span> <span className="hospclr">Hospitals, </span> <span className="cliniclr">Clinics, </span> <span className="labsclr">Labs, </span> <span className="otherclr">near you </span></h1>
                     </div>
                 </div>
+                <div className="row">
+                    <div className="col-8 offset-2">
+                        
+                    </div> 
+                </div>
+                
             </div>
             
         </div>
